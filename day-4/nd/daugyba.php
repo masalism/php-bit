180d06cff3c2e0082595eb38bf83a93761ecc123
Mantas Masalis <33566613+masalism@users.noreply.github.com>
2020-04-06 15:13:28 +0300

    daugyba

--- day-4/nd/daugyba.php
+++ day-4/nd/daugyba.php
@@ -1,10 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        .lentele {
+            display: flex;
+            justify-content: space-between;
+        }
+
+        .stulpelis {
+            display: flex;
+            flex-direction: column;
+        }
+    </style>
     <title>Document</title>
 </head>
+
 <body>
 
     <form action="daugyba.php" method="POST">
@@ -20,12 +33,20 @@
         echo "$num * $i = " . $num * $i . "<br>";
     }
 
-    for ($i = 1; $i <=10; $i++) {
-        for ($j = 1; $j <=10; $j++) {
-            echo "$i * $j = " .  $i * $j . "<br>";
-        }
-    }
-    
     ?>
+
+    <div class="lentele">
+        <?php
+
+        for ($i = 1; $i <= 10; $i++) {
+            echo "<div class=\"stulpelis\">";
+            for ($j = 1; $j <= 10; $j++) {
+                echo "<p>$i * $j = " .  $i * $j . "</p>";
+            }
+            echo "</div>";
+        }
+        ?>
+    </div>
 </body>
+
 </html>
