cb6a40ec97af41183cb271fb168ea9ce11f54c55
Mantas Masalis <33566613+masalism@users.noreply.github.com>
2020-04-13 17:03:28 +0300

    file browser

--- FileBrowser/code.php
+++ FileBrowser/code.php
@@ -1,27 +1,168 @@
-<?php
-$dir = "../.." . $_SERVER['REQUEST_URI'];
+<!DOCTYPE html>
+<html lang="en">
 
-$files = scandir($dir);
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <title>File Browser</title>
+</head>
 
-foreach ($files as $value) {
-    $type;
-    $newValue;
+<body>
+    <div class="container">
+        <?php
+        session_start();
+        // Jei neprisijunges, grazinam i index.php
+        if (!$_SESSION['logged_in']) {
+            header("Location: index.php");
+            exit;
+        }
 
-    if (is_dir($value)) {
-        $type = "Directory";
-        $newValue = "<a href=\"$dir/$value\">$value</a>";
-    } else if (is_file($value)) {
-        $type = "File";
-        $newValue = $value;
-    }
+        // log out
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            if (isset($_POST['logout'])) {
+                session_start();
+                unset($_SESSION['username']);
+                unset($_SESSION['password']);
+                unset($_SESSION['logged_in']);
+                header("Location: index.php");
+            }
+        }
 
-    if ($value == "." || $value == "..") {
-        $value = null;
-    } else {
-        print("<tr>
-                   <td>$type</td>
-                   <td>$newValue</td> 
-                   <td></td> 
-               </tr>");
-    }
-}
+        // Failo ikelimas
+        $errorMessage = '';
+
+        if (isset($_FILES['file'])) {
+            $file = $_FILES['file'];
+            $ext = strtolower(end(explode('.', $_FILES['file']['name'])));
+
+            if ($file['size'] > 5 * 1024 * 1024) {
+                $errorMessage = "You can not upload more than 5 mb files";
+            } else if (!in_array($ext, ['png', 'jpg', 'svg', 'jpeg'])) {
+                $errorMessage = "You can only upload images";
+            } else {
+                move_uploaded_file($_FILES['file']['tmp_name'], "./" . $_GET['path'] . $_FILES['file']['name']);
+            }
+        }
+
+        // Failo istrinimas ir dir kurimas
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            if (isset($_POST['delete'])) {
+                $fileDelete = './' . $_GET['path'] . $_POST['delete'];
+                if (is_file($fileDelete)) {
+                    unlink($fileDelete);
+                }
+            } elseif (isset($_POST['create'])) {
+                if ($_POST['create'] != "") {
+                    $dirCreate = './' . $_GET['path'] . $_POST['create'];
+                    if (!is_dir($dirCreate))
+                        mkdir($dirCreate, 0777, true);
+                }
+            }
+        }
+         // failo parsisiuntimas
+         if (isset($_POST['download'])) {
+            print('Path to download: ' . './' . $_GET["path"] . $_POST['download']);
+            $file = './' . $_GET["path"] . $_POST['download'];
+            $fileToDownloadEscaped = str_replace("&nbsp;", " ", htmlentities($file, null, 'utf-8'));
+            header('Content-Description: File Transfer');
+            header('Content-Type: application/pdf');
+            header('Content-Disposition: attachment; filename=' . basename($fileToDownloadEscaped));
+            header('Content-Transfer-Encoding: binary');
+            header('Expires: 0');
+            header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+            header('Pragma: public');
+            header('Content-Length: ' . filesize($fileToDownloadEscaped));
+            flush();
+            readfile($fileToDownloadEscaped);
+            exit;
+        }
+
+        // Direktorijos spausdinimas
+        $path = './' . $_GET["path"];
+        $data = scandir($path);
+
+        echo '<h2 class="mt-3">Directory: ' . str_replace('?path=', '/', $_SERVER['REQUEST_URI']) . "</h2>";
+
+        ?>
+        
+    </div>
+    <div class="container">
+        <?php
+
+        echo "<table class=\"table table-striped mt-5\"><thead class=\"thead-dark\"><tr><th>Type</th><th>Name<th>Action</th></th></thead>";
+        echo "<tbody>";
+
+        foreach ($data as $value) {
+            $newValue = (is_dir($path . $value)
+                ? '<a href="' . (isset($_GET['path'])
+                    ?  $_SERVER['REQUEST_URI'] . $value . '/'
+                    : $_SERVER['REQUEST_URI'] . '?path=' . $value . '/') . '">' . $value . '</a>'
+                : $value);
+            $type = is_dir($path . $value) ? "Directory" : "File";
+            $options = is_dir($path . $value) ? null : 
+            '<form action=" " method="POST">
+                <input type="hidden" name="delete" value=' . $value . '>
+                <button type="submit" class="btn btn-danger btn-sm mr-2">Delete</button>
+            </form>
+            <form action="" method="POST">
+                <input type="hidden" name="download" value=' . $value . '>
+                <button type="submit" class="btn btn-primary btn-sm">Download</button>
+            </form>';
+
+            if ($value != ".." && $value != '.') {
+                echo "<tr>";
+                echo "<td>$type</td>";
+                echo "<td>$newValue</td>";
+                echo "<td class=\"d-flex\">$options</td>";
+                echo "</tr>";
+            }
+        }
+
+        echo "</tbody>";
+        echo "</table>";
+
+        ?>
+        
+        <?php
+        // "Grizimo" kodas
+        $queryString = explode("/", $_SERVER['QUERY_STRING']);
+        array_pop($queryString);
+        if (count($queryString) == 1) {
+            $urlString = explode("?", $_SERVER['REQUEST_URI']);
+            array_pop($urlString);
+            $newURL = implode("/", $urlString);
+        } else {
+            $urlString = explode("/", $_SERVER['REQUEST_URI']);
+            array_pop($urlString);
+            array_pop($urlString);
+            $newURL = implode("/", $urlString) . "/";
+        }
+        ?>
+
+        <a href="<?php print($newURL) ?>">
+            <button class="btn btn-success">Grįžti</button>
+        </a>
+
+        <form class="form-group mt-3 border rounded w-50 bg-light" action="" method="post" enctype="multipart/form-data">
+            <input class="form-control-file" type="file" name="file"><br>
+            <button class="btn btn-warning">Įkelti</button>
+            <h3><?php echo $errorMessage ?></h3>
+        </form>
+
+        <form class="w-50 my-3" action=" " method="POST">
+            <input class="form-control mb-2" name="create" placeholder="Direktorijos pavadinimas">
+            <button type="submit" class="btn btn-secondary">Sukurti direktoriją</button>
+        </form>
+
+        <form class="logout" action="code.php" method="POST">
+            <input type="hidden" name="logout">
+            <button type="submit" class="btn btn-danger btn-lg mt-2">Atsijungti</button>
+        </form>
+    </div>
+
+
+
+</body>
+
+</html>
--- FileBrowser/index.php
+++ FileBrowser/index.php
@@ -4,81 +4,48 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous" />
+    <title>Login to File Browser</title>
 </head>
 
 <body>
+    <h1 class="h1 text-center mt-5">Log In</h1>
 
-    <?php
+    <div class="container d-flex justify-content-center mt-5">
+        <form action="" method="POST" class="col-lg-4 col-sm-8 col-12">
+            <div class="form-group">
+                <label for="username">Username</label>
+                <input type="text" class="form-control" name="username" placeholder="mantas">
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input type="password" class="form-control" name="password" placeholder="test123">
+            </div>
+            <?php
+            // login
+            session_start();
+            $msg = '';
+            if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+                if (isset($_POST['login'])) {
+                    if ($_POST['username'] == "mantas" && $_POST['password'] == "test123") {
+                        $_SESSION['logged_in'] = true;
+                        $_SESSION['timeout'] = time();
+                        $_SESSION['username'] = "mantas";
+                        header("Location: code.php");
+                    } else {
+                        $msg = 'Neteisingi prisijungimo duomenys';
+                    }
+                }
+            }
 
-    $path = './' . $_GET["path"];
-    $data = scandir($path);
-
-    echo '<h2>Directory: ' . str_replace('?path=', '', $_SERVER['REQUEST_URI']) . "</h2>";
-
-    echo "<table style=\"width: 100%\" border=\"1\"><thead><tr><th>Type</th><th>Name<th>Action</th></th></thead>";
-    echo "<tbody>";
-
-    foreach ($data as $value) {
-        $newValue = (is_dir($path . $value) 
-        ? '<a href="' . (isset($_GET['path']) 
-            ? $_SERVER['REQUEST_URI'] . $value . '/' 
-            : $_SERVER['REQUEST_URI'] . '?path=' . $value . '/' ) . '">' . $value . '</a>' 
-        : $value);
-        $type = is_dir($path . $value) ? "Directory" : "File";
-        $options = is_dir($path . $value) ? null : '<button>Delete</button><button>Download</button>';
-
-        if ($value != ".." && $value != '.') {
-            echo "<tr>";
-            echo "<td>$type</td>";
-            echo "<td>$newValue</td>";
-            echo "<td>$options</td>";
-            echo "</tr>";
-        }
-    }
-
-    echo "</tbody>";
-    echo "</table>";
-
-    
-        // if (is_dir($path . $value)) {
-        //     // $options = null;
-        //     if (isset($GET['path'])) {
-        //         $newValue = '<a href="' . $_SERVER['REQUEST_URI'] . $value . '/' . '">' . $value . '</a>';
-        //     } else {
-        //         $newValue = '<a href="' . $_SERVER['REQUEST_URI'] . '?path=' . $value . '/' . '">' . $value . '</a>';
-        //     }
-        // } else {
-        //     $newValue = $value;
-        //     // $options = "<button>Delete</button><button>Download</button>";
-        // }
-
-    // foreach ($files as $value) {
-    //     $type;
-    //     $newValue;
-
-    //     if (is_dir($value)) {
-    //         $type = "Directory";
-    //         $newValue = "<a href=\"$dir/$value\">$value</a>";
-    //     } else if (is_file($value)) {
-    //         $type = "File";
-    //         $newValue = $value;
-    //     }
-
-    //     if ($value == "." || $value == "..") {
-    //         $value = null;
-    //     } else {
-    //         print("<tr>
-    //                    <td>$type</td>
-    //                    <td>$newValue</td> 
-    //                    <td></td> 
-    //                </tr>");
-    //     }
-
-    ?>
+            echo  "<p class=\"text-danger display-5 \">$msg</p>";
+            ?>
+            <input class="btn btn-primary mt-2 w-100" type="submit" value="Prisijungti" name="login">
+           
+        </form>
 
 
-
+    </div>
 </body>
 
 </html>
